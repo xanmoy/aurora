ea74cf68f39056859b95a513a7ddff2bb16a9304
Tanmoy Ganguly <contact@xanmoy.me>
2024-07-05 04:03:38 +0530

    Update aurora.js

--- src/lib/aurora.js
+++ src/lib/aurora.js
@@ -12,7 +12,7 @@ import {
 const MODEL_NAME = "gemini-1.0-pro";
 
 async function runChat(prompt) {
-  const API_KEY = process.env.API_KEY || 'AIzaSyCLEh4LzvaQyRFBYEOlncZW5Ls_N1gS0sk';
+  const API_KEY = process.env.API_KEY || 'AIzaSyDcTChviRxm2sHeypgJ44INUyYMIwnLRbs';
 
   const genAI = new GoogleGenerativeAI(API_KEY);
   const model = genAI.getGenerativeModel({ model: MODEL_NAME });
